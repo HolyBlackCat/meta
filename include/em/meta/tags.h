165c3ed93c0e6ba1c9d268e26a48f2908dbfea42
HolyBlackCat <holyblckcat@gmail.com>
2024-10-12 18:00:59 +0500

    Comment.

--- include/em/meta/tags.h
+++ include/em/meta/tags.h
@@ -4,6 +4,8 @@
 
 namespace em::Meta
 {
+    // A tag. This can be used for ADL dispatch, which is intentional (`foo(tag<T>)` will search the namespace of `T` with ADL).
     template <typename T> struct tag {using type = T;};
+
     template <auto N> struct value_tag {static constexpr auto value = N;};
 }
