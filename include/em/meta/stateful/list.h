82d5e5792aefc54f67542dad5bda64706a813a41
HolyBlackCat <holyblckcat@gmail.com>
2025-02-01 12:48:39 +0500

    Use exponential search to compute size of stateful lists.

--- include/em/meta/stateful/list.h
+++ include/em/meta/stateful/list.h
@@ -33,11 +33,44 @@ namespace em::Meta::Stateful
 
             constexpr void _adl_em_StatefulListElem() {} // A dummy ADL target.
 
-            template <typename Name, std::size_t Index, typename Unique, typename = void>
+            // We have two different strategies to compute the list size, a linear search and an exponential search.
+            // The latter is faster on very large lists, but could in theory have more overhead on many small lists.
+            #ifndef EM_STATEFUL_LIST_USE_EXPONENTIAL_SEARCH
+            #define EM_STATEFUL_LIST_USE_EXPONENTIAL_SEARCH 1
+            #endif
+            #if !EM_STATEFUL_LIST_USE_EXPONENTIAL_SEARCH // Naive linear search
+            template <typename Name, typename Unique, std::size_t Index = 0, typename = void>
             struct CalcSize : std::integral_constant<std::size_t, Index> {};
+            template <typename Name, typename Unique, std::size_t Index>
+            struct CalcSize<Name, Unique, Index, decltype(void(_adl_em_StatefulListElem(ElemReader<Name, Index>{})))> : CalcSize<Name, Unique, Index + 1> {};
+            #else // Exponential search.
+            // See below.
+            template <typename Name, typename Unique, std::size_t Low, std::size_t High>
+            struct CalcSize1;
 
-            template <typename Name, std::size_t Index, typename Unique>
-            struct CalcSize<Name, Index, Unique, decltype(void(_adl_em_StatefulListElem(ElemReader<Name, Index>{})))> : CalcSize<Name, Index + 1, Unique> {};
+            // Checks if `Mid` is a valid index or not.
+            // Calls `CalcSize1` with the correct half of the range.
+            template <typename Name, typename Unique, std::size_t Low, std::size_t High, std::size_t Mid, typename = void>
+            struct CalcSize2 : CalcSize1<Name, Unique, Low, Mid> {};
+            template <typename Name, typename Unique, std::size_t Low, std::size_t High, std::size_t Mid>
+            struct CalcSize2<Name, Unique, Low, High, Mid, decltype(void(_adl_em_StatefulListElem(ElemReader<Name, Mid>{})))> : CalcSize1<Name, Unique, Mid, High> {};
+
+            // If the range has size 1 or 0, returns the end of the range. Otherwise passes it to `CalcSize2`.
+            // The size range 0 is only possible if the entire range we're operating on is empty.
+            template <typename Name, typename Unique, std::size_t Low, std::size_t High>
+            struct CalcSize1 : CalcSize2<Name, Unique, Low, High, (Low + High) / 2> {};
+            template <typename Name, typename Unique, std::size_t Low>
+            struct CalcSize1<Name, Unique, Low, Low> : std::integral_constant<std::size_t, Low> {};
+            template <typename Name, typename Unique, std::size_t Low>
+            struct CalcSize1<Name, Unique, Low, Low + 1> : std::integral_constant<std::size_t, Low + 1> {};
+
+            // Checks the powers of two.
+            // When we find one that's not a valid index, passes the two latest numbers to `CalcSize1`.
+            template <typename Name, typename Unique, std::size_t Low = 0, std::size_t High = 0, typename = void>
+            struct CalcSize : CalcSize1<Name, Unique, Low, High> {};
+            template <typename Name, typename Unique, std::size_t Low, std::size_t High>
+            struct CalcSize<Name, Unique, Low, High, decltype(void(_adl_em_StatefulListElem(ElemReader<Name, High>{})))> : CalcSize<Name, Unique, High, High ? High * 2 : 1> {};
+            #endif
 
             template <typename Name, std::size_t Index, typename Unique>
             using ReadElem = typename decltype(_adl_em_StatefulListElem(ElemReader<Name, Index>{}))::type;
@@ -55,7 +88,7 @@ namespace em::Meta::Stateful
 
         // Calculates the current list size.
         template <typename Name, typename Unique = DefaultUnique>
-        inline constexpr std::size_t size = detail::CalcSize<Name, 0, Unique>::value;
+        inline constexpr std::size_t size = detail::CalcSize<Name, Unique>::value;
 
         // Touch this type to append `Value` to the list.
         template <typename Name, typename Value, typename Unique = DefaultPushBackUnique<Value>>
