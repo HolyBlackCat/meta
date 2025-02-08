ab6db6bda7460f528e60a61b1055a22f08785672
HolyBlackCat <holyblckcat@gmail.com>
2025-02-08 14:03:33 +0500

    Rework and add some things to `cvref.h`.

--- include/em/meta/cvref.h
+++ include/em/meta/cvref.h
@@ -1,84 +1,82 @@
 #pragma once
 
+#include "em/macros/utils/implies.h"
+#include "em/macros/utils/flag_enum.h"
+
 #include <concepts>
 #include <type_traits>
+#include <typeinfo>
 
 // Manipulating cvref-qualifiers.
 
 namespace em::Meta
 {
-    // Check lack of qualifiers.
+    // --- Check lack of qualifiers.
 
     template <typename T> concept cv_unqualified = std::is_same_v<std::remove_cv_t<T>, T>;
     template <typename T> concept cvref_unqualified = std::is_same_v<std::remove_cvref_t<T>, T>;
 
 
-    // Add qualifiers.
+    // --- Add qualifiers.
 
     namespace detail
     {
-        template <typename T> struct cvref_const {using type = const T;};
-        template <typename T> struct cvref_const<T &> {using type = const T &;};
-        template <typename T> struct cvref_const<T &&> {using type = const T &&;};
+        template <typename T> struct const_type {using type = const T;};
+        template <typename T> struct const_type<T &> {using type = const T &;};
+        template <typename T> struct const_type<T &&> {using type = const T &&;};
     }
 
-    // Exactly like `std::add_const_t`.
-    template <typename T> using cv_const = const T;
+    // Exactly like `const T` and `std::add_const_t`, has no effect on references.
+    template <typename T> using const_type = const T;
 
-    // Given `T [const] [volatile] [&|&&]`, returns `T const [volatile] [&|&&]`.
-    // Unlike `std::add_const_t<T>` and `const T`, this injects constness into the referenced type for references.
-    template <typename T> using cvref_const = typename detail::cvref_const<T>::type;
+    // Like `const T`, but when applied to a reference, modifies the referenced type.
+    template <typename T> using const_type_cvref = typename detail::const_type<T>::type;
 
     // Forwards `x` with constness added. Unlike `std::as_const()`, this can also handle rvalues.
     inline constexpr auto make_const = []<typename T> [[nodiscard]] (T &&x) -> auto &&
     {
-        return static_cast<cvref_const<T &&>>(x);
+        return static_cast<const_type<T &&>>(x);
     };
 
-    // If `T` is a non-reference, make it a const rvalue reference.
-    template <typename T>
-    using nonref_to_const_rvalue_ref = std::conditional_t<std::is_reference_v<T>, T, const T &&>;
 
-
-    // Conditional constness.
-    // `maybe_const<true, T>` is `const T`, and `maybe_const<false, T>` is just `T`.
+    // --- Conditional constness.
 
     namespace detail
     {
-        template <bool Const, typename T>
-        struct maybe_const {using type = T;};
+        template <bool Const, typename T> struct maybe_const {using type = T;};
+        template <typename T> struct maybe_const<true, T> {using type = const T;};
 
-        template <typename T>
-        struct maybe_const<true, T> {using type = const T;};
+        template <bool Const, typename T> struct maybe_const_cvref {using type = T;};
+        template <typename T> struct maybe_const_cvref<true, T> {using type = typename const_type<T>::type;};
     }
 
+    // If `Const`, applies `const_type` to `T`, otherwise leaves it unchanged.
     template <bool Const, typename T>
     using maybe_const = typename detail::maybe_const<Const, T>::type;
 
+    // If `Const`, applies `const_type_cvref` to `T`, otherwise leaves it unchanged.
+    template <bool Const, typename T>
+    using maybe_const_cvref = typename detail::maybe_const_cvref<Const, T>::type;
 
-    // Copy qualifiers from one type to another.
-    // Given `cvA refA A` and `cvB refB B`:
-    // * `copy_cv`    returns `cvA refB B`
-    // * `copy_cvref` returns `cvA refA B`
+
+    // --- Copy qualifiers from one type to another.
 
     namespace detail
     {
-        template <typename A, typename B> struct copy_cv_low                      {using type =                B;};
-        template <typename A, typename B> struct copy_cv_low<const          A, B> {using type = const          B;};
-        template <typename A, typename B> struct copy_cv_low<      volatile A, B> {using type =       volatile B;};
-        template <typename A, typename B> struct copy_cv_low<const volatile A, B> {using type = const volatile B;};
+        template <typename A, typename B> struct copy_cv_impl                      {using type =                B;};
+        template <typename A, typename B> struct copy_cv_impl<const          A, B> {using type = const          B;};
+        template <typename A, typename B> struct copy_cv_impl<      volatile A, B> {using type =       volatile B;};
+        template <typename A, typename B> struct copy_cv_impl<const volatile A, B> {using type = const volatile B;};
 
-        template <typename A, typename B> struct copy_cv          {using type = typename copy_cv_low<A, std::remove_cvref_t<B>>::type;};
-        template <typename A, typename B> struct copy_cv<A, B & > {using type = typename copy_cv_low<A, std::remove_cvref_t<B>>::type &;};
-        template <typename A, typename B> struct copy_cv<A, B &&> {using type = typename copy_cv_low<A, std::remove_cvref_t<B>>::type &&;};
-
-        template <typename A, typename B> struct copy_cvref          {using type = typename copy_cv_low<A, std::remove_cvref_t<B>>::type;};
-        template <typename A, typename B> struct copy_cvref<A & , B> {using type = typename copy_cv_low<A, std::remove_cvref_t<B>>::type &;};
-        template <typename A, typename B> struct copy_cvref<A &&, B> {using type = typename copy_cv_low<A, std::remove_cvref_t<B>>::type &&;};
+        template <typename A, typename B> struct copy_cvref_impl          {using type = typename copy_cv_impl<A, B>::type;};
+        template <typename A, typename B> struct copy_cvref_impl<A & , B> {using type = typename copy_cv_impl<A, B>::type &;};
+        template <typename A, typename B> struct copy_cvref_impl<A &&, B> {using type = typename copy_cv_impl<A, B>::type &&;};
     }
 
-    template <typename A, typename B> using copy_cv = typename detail::copy_cv<std::remove_reference_t<A>, B>::type;
-    template <typename A, typename B> using copy_cvref = typename detail::copy_cvref<A, B>::type;
+    // Ignores referenceness on `A`, adds its cv-qualifiers to `B`. Naturally has no effect if `B` is a reference.
+    template <typename A, typename B> using copy_cv    = typename detail::copy_cv_impl<std::remove_reference_t<A>, B>::type;
+    // Adds cvref-qualifiers from `A` to `B`. If `B` is a reference, cv-qualifiers are ignored and the normal reference collapsing rules apply.
+    template <typename A, typename B> using copy_cvref = typename detail::copy_cvref_impl<A, B>::type;
 
 
     // Comparison ignoring qualifiers.
@@ -87,18 +85,98 @@ namespace em::Meta
     template <typename A, typename B> concept same_ignoring_cvref = std::same_as<std::remove_cvref_t<A>, std::remove_cvref_t<B>>;
 
 
+    // Representing cvref as flags.
+
+    enum class CvrefFlags : unsigned char
+    {
+        none = 0,
+        const_ = 0b001,
+        lvalue_ref = 0b010,
+        rvalue_ref = 0b100,
+        const_lvalue_ref = const_ | lvalue_ref,
+        const_rvalue_ref = const_ | rvalue_ref,
+
+        // Not a valid value, use this mask to extract reference-ness.
+        ref_mask = lvalue_ref | rvalue_ref,
+
+        // All valid values are less then than this.
+        end_value = 0b110,
+    };
+    EM_FLAG_ENUM(CvrefFlags)
+
     namespace detail
     {
-        template <typename A, typename B>
-        struct same_ignoring_cvref_and_convertible : std::false_type {};
+        template <typename T> struct QualToFlags             : std::integral_constant<CvrefFlags, CvrefFlags::none> {};
+        template <typename T> struct QualToFlags<const T   > : std::integral_constant<CvrefFlags, CvrefFlags::const_> {};
+        template <typename T> struct QualToFlags<      T & > : std::integral_constant<CvrefFlags, CvrefFlags::lvalue_ref> {};
+        template <typename T> struct QualToFlags<const T & > : std::integral_constant<CvrefFlags, CvrefFlags::const_lvalue_ref> {};
+        template <typename T> struct QualToFlags<      T &&> : std::integral_constant<CvrefFlags, CvrefFlags::rvalue_ref> {};
+        template <typename T> struct QualToFlags<const T &&> : std::integral_constant<CvrefFlags, CvrefFlags::const_rvalue_ref> {};
 
-        template <typename A, typename B> requires same_ignoring_cvref<A, B>
-        struct same_ignoring_cvref_and_convertible<A, B> : std::is_convertible<A, B> {};
+        template <CvrefFlags F, typename T> struct FlagsToQual {};
+        template <typename T> struct FlagsToQual<CvrefFlags::none            , T> {using type =       T;};
+        template <typename T> struct FlagsToQual<CvrefFlags::const_          , T> {using type = const T;};
+        template <typename T> struct FlagsToQual<CvrefFlags::lvalue_ref      , T> {using type =       T &;};
+        template <typename T> struct FlagsToQual<CvrefFlags::const_lvalue_ref, T> {using type = const T &;};
+        template <typename T> struct FlagsToQual<CvrefFlags::rvalue_ref      , T> {using type =       T &&;};
+        template <typename T> struct FlagsToQual<CvrefFlags::const_rvalue_ref, T> {using type = const T &&;};
     }
 
-    // What it says on the tin.
-    // Returns true if `A` and `B` are the same ignoring cvref-qualifiers, AND if A is convertible to B.
-    // Here `same_ignoring_cvref<A, B>` is redundant (but added for subsumption), and the implementation (`same_ignoring_cvref_and_convertible`)
-    //   checks it again to hopefully avoid checking convertability when not needed.
-    template <typename A, typename B> concept same_ignoring_cvref_and_convertible = same_ignoring_cvref<A, B> && detail::same_ignoring_cvref_and_convertible<A, B>::value;
+    // Extracts cvref-qualifiers from a type to flags.
+    template <typename T> constexpr CvrefFlags cvref_to_flags = detail::QualToFlags<T>::value;
+
+    // Adds cvref-qualifiers from flags to a type. If the target is a reference, constness is ignored, and the normal reference collapsing rules apply.
+    template <CvrefFlags F, typename T> using flags_to_cvref = typename detail::FlagsToQual<F, T>::type;
+
+
+    // Combines `CvrefFlags` and `std::type_info`.
+    struct CvrefFlagsAndType
+    {
+        CvrefFlags flags{};
+
+        // This should be `std::type_index` by value, but that currently doesn't have a constexpr `operator==`.
+        const std::type_info *type = nullptr;
+    };
+
+    // Returns `CvrefFlags` and `std::type_info` for a type.
+    template <typename T>
+    constexpr CvrefFlagsAndType type_to_desc = {.flags = cvref_to_flags<T>, .type = &typeid(T)};
+
+
+    // Checks that `from` flags are convertible to `to` flags.
+    // If `to` is a non-ref, this version always returns false (you must check for that manually, taking into account copyability/movability of the type).
+    // If `from` is a non-ref, it's assumed to be an rvalue ref. You can adjust it manually before calling if you don't like this.
+    [[nodiscard]] constexpr bool CvrefFlagsConvertible(CvrefFlags from, CvrefFlags to)
+    {
+        if (!bool(to & CvrefFlags::ref_mask))
+            return false; // See comment above.
+        if (to == CvrefFlags::const_lvalue_ref)
+            return true;
+        return
+            // `from` const implies `to` const
+            (bool(from & CvrefFlags::const_) EM_IMPLIES bool(to & CvrefFlags::const_)) &&
+            // If `from` is rvalue ref or non-ref, `to` must be rvalue ref (non-ref was rejected before). Or both must be lvalue refs.
+            ((from & CvrefFlags::ref_mask) == CvrefFlags::lvalue_ref) == ((to & CvrefFlags::ref_mask) == CvrefFlags::lvalue_ref);
+    }
+
+    // Checks that `desc` describes the same type as `desc` and the cvref-qualifiers are compatible.
+    // If `from` is a non-ref, it's assumed to be an rvalue ref. You can adjust it manually before calling if you don't like this.
+    template <typename T>
+    [[nodiscard]] constexpr bool SameTypeAndConstructibleFromDesc(const CvrefFlagsAndType &desc)
+    {
+        if (*desc.type != typeid(T))
+            return false;
+
+        if constexpr (std::is_reference_v<T>)
+        {
+            return CvrefFlagsConvertible(desc.flags, cvref_to_flags<T>);
+        }
+        else
+        {
+            if (bool(desc.flags & CvrefFlags::const_))
+                return (desc.flags & CvrefFlags::ref_mask) == CvrefFlags::lvalue_ref ? std::is_constructible_v<T, const T &> : std::is_constructible_v<T, const T &&>;
+            else
+                return (desc.flags & CvrefFlags::ref_mask) == CvrefFlags::lvalue_ref ? std::is_constructible_v<T, T &> : std::is_constructible_v<T, T &&>;
+        }
+    }
 }
---
+++ test/cvref.cpp
@@ -0,0 +1,130 @@
+#include "em/meta/cvref.h"
+
+// copy_cv, copy_cvref
+
+#define TEST(CV1, CV2, CV12) \
+    static_assert(std::is_same_v<em::Meta::copy_cv           <CV1 int   , CV2 float   >, CV12 float   >); \
+    static_assert(std::is_same_v<em::Meta::copy_cv           <CV1 int   , CV2 float & >, CV2  float & >); \
+    static_assert(std::is_same_v<em::Meta::copy_cv           <CV1 int   , CV2 float &&>, CV2  float &&>); \
+    static_assert(std::is_same_v<em::Meta::copy_cv           <CV1 int & , CV2 float   >, CV12 float   >); \
+    static_assert(std::is_same_v<em::Meta::copy_cv           <CV1 int & , CV2 float & >, CV2  float & >); \
+    static_assert(std::is_same_v<em::Meta::copy_cv           <CV1 int & , CV2 float &&>, CV2  float &&>); \
+    static_assert(std::is_same_v<em::Meta::copy_cv           <CV1 int &&, CV2 float   >, CV12 float   >); \
+    static_assert(std::is_same_v<em::Meta::copy_cv           <CV1 int &&, CV2 float & >, CV2  float & >); \
+    static_assert(std::is_same_v<em::Meta::copy_cv           <CV1 int &&, CV2 float &&>, CV2  float &&>); \
+    static_assert(std::is_same_v<em::Meta::copy_cvref        <CV1 int   , CV2 float   >, CV12 float   >); \
+    static_assert(std::is_same_v<em::Meta::copy_cvref        <CV1 int   , CV2 float & >, CV2  float & >); \
+    static_assert(std::is_same_v<em::Meta::copy_cvref        <CV1 int   , CV2 float &&>, CV2  float &&>); \
+    static_assert(std::is_same_v<em::Meta::copy_cvref        <CV1 int & , CV2 float   >, CV12 float & >); \
+    static_assert(std::is_same_v<em::Meta::copy_cvref        <CV1 int & , CV2 float & >, CV2  float & >); \
+    static_assert(std::is_same_v<em::Meta::copy_cvref        <CV1 int & , CV2 float &&>, CV2  float & >); \
+    static_assert(std::is_same_v<em::Meta::copy_cvref        <CV1 int &&, CV2 float   >, CV12 float &&>); \
+    static_assert(std::is_same_v<em::Meta::copy_cvref        <CV1 int &&, CV2 float & >, CV2  float & >); \
+    static_assert(std::is_same_v<em::Meta::copy_cvref        <CV1 int &&, CV2 float &&>, CV2  float &&>); \
+
+TEST(              ,               ,               )
+TEST(const         ,               , const         )
+TEST(      volatile,               ,       volatile)
+TEST(const volatile,               , const volatile)
+TEST(              , const         , const         )
+TEST(const         , const         , const         )
+TEST(      volatile, const         , const volatile)
+TEST(const volatile, const         , const volatile)
+TEST(              ,       volatile,       volatile)
+TEST(const         ,       volatile, const volatile)
+TEST(      volatile,       volatile,       volatile)
+TEST(const volatile,       volatile, const volatile)
+TEST(              , const volatile, const volatile)
+TEST(const         , const volatile, const volatile)
+TEST(      volatile, const volatile, const volatile)
+TEST(const volatile, const volatile, const volatile)
+#undef TEST
+
+static_assert(std::is_same_v<em::Meta::const_type<               int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::const_type<const          int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::const_type<      volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::const_type<const volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::const_type<               int & >,                int & >);
+static_assert(std::is_same_v<em::Meta::const_type<const          int & >, const          int & >);
+static_assert(std::is_same_v<em::Meta::const_type<      volatile int & >,       volatile int & >);
+static_assert(std::is_same_v<em::Meta::const_type<const volatile int & >, const volatile int & >);
+static_assert(std::is_same_v<em::Meta::const_type<               int &&>,                int &&>);
+static_assert(std::is_same_v<em::Meta::const_type<const          int &&>, const          int &&>);
+static_assert(std::is_same_v<em::Meta::const_type<      volatile int &&>,       volatile int &&>);
+static_assert(std::is_same_v<em::Meta::const_type<const volatile int &&>, const volatile int &&>);
+
+static_assert(std::is_same_v<em::Meta::maybe_const<false,                int   >,                int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const<false, const          int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const<false,       volatile int   >,       volatile int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const<false, const volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const<false,                int & >,                int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const<false, const          int & >, const          int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const<false,       volatile int & >,       volatile int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const<false, const volatile int & >, const volatile int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const<false,                int &&>,                int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const<false, const          int &&>, const          int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const<false,       volatile int &&>,       volatile int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const<false, const volatile int &&>, const volatile int &&>);
+
+static_assert(std::is_same_v<em::Meta::maybe_const<true,                int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const<true, const          int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const<true,       volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const<true, const volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const<true,                int & >,                int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const<true, const          int & >, const          int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const<true,       volatile int & >,       volatile int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const<true, const volatile int & >, const volatile int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const<true,                int &&>,                int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const<true, const          int &&>, const          int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const<true,       volatile int &&>,       volatile int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const<true, const volatile int &&>, const volatile int &&>);
+
+static_assert(std::is_same_v<em::Meta::const_type_cvref<               int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<const          int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<      volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<const volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<               int & >, const          int & >);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<const          int & >, const          int & >);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<      volatile int & >, const volatile int & >);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<const volatile int & >, const volatile int & >);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<               int &&>, const          int &&>);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<const          int &&>, const          int &&>);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<      volatile int &&>, const volatile int &&>);
+static_assert(std::is_same_v<em::Meta::const_type_cvref<const volatile int &&>, const volatile int &&>);
+
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false,                int   >,                int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false, const          int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false,       volatile int   >,       volatile int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false, const volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false,                int & >,                int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false, const          int & >, const          int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false,       volatile int & >,       volatile int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false, const volatile int & >, const volatile int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false,                int &&>,                int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false, const          int &&>, const          int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false,       volatile int &&>,       volatile int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<false, const volatile int &&>, const volatile int &&>);
+
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true,                int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true, const          int   >, const          int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true,       volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true, const volatile int   >, const volatile int   >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true,                int & >, const          int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true, const          int & >, const          int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true,       volatile int & >, const volatile int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true, const volatile int & >, const volatile int & >);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true,                int &&>, const          int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true, const          int &&>, const          int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true,       volatile int &&>, const volatile int &&>);
+static_assert(std::is_same_v<em::Meta::maybe_const_cvref<true, const volatile int &&>, const volatile int &&>);
+
+
+
+// ---
+
+
+
+static_assert(em::Meta::SameTypeAndConstructibleFromDesc<int>(em::Meta::type_to_desc<int>));
+static_assert(em::Meta::SameTypeAndConstructibleFromDesc<int &>(em::Meta::type_to_desc<int &>));
+static_assert(!em::Meta::SameTypeAndConstructibleFromDesc<int &>(em::Meta::type_to_desc<int &&>));
+static_assert(!em::Meta::SameTypeAndConstructibleFromDesc<int>(em::Meta::type_to_desc<float>));
