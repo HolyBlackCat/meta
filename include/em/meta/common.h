8bbf312d2f61de50b2de3dc5fefc7fa2a7fb7146
HolyBlackCat <holyblckcat@gmail.com>
2025-07-13 00:26:17 +0500

    Move `specialization_of` to `common.h`.

--- include/em/meta/common.h
+++ include/em/meta/common.h
@@ -159,4 +159,23 @@ namespace em::Meta
     // If all template parameters are the same type, returns that type. Otherwise fails.
     template <typename ...P> requires same_as_all<P...>
     using require_same_type = first_type<P...>;
+
+
+    // --- Specializations.
+
+    namespace detail
+    {
+        template <typename T, template <typename...> typename U> struct SpecializationOf : std::false_type {};
+        template <typename ...P, template <typename...> typename U> struct SpecializationOf<U<P...>, U> : std::true_type {};
+    }
+
+    // Returns true if `T` is a `U<...>`, ignoring cvref-qualifiers.
+    // This only works if `U` only has type template parameters. It appears to be impossible to support arbitrary types.
+    template <typename T, template <typename...> typename U>
+    concept specialization_of_ignoring_cvref = detail::SpecializationOf<std::remove_cvref_t<T>, U>::value;
+
+    // Returns true if `T` is a `U<...>` (cvref-unqualified).
+    // This only works if `U` only has type template parameters. It appears to be impossible to support arbitrary types.
+    template <typename T, template <typename...> typename U>
+    concept specialization_of = cvref_unqualified<T> && specialization_of_ignoring_cvref<T, U>;
 }
